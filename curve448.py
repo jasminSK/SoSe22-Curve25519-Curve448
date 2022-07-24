6e1849532faf362eeb31b7a537add3c8478b1caa
lh12318 <lisa.huettinger@stud.th-deg.de>
2022-07-24 23:32:50 +0200

    Added comments

--- curve448.py
+++ curve448.py
@@ -2,7 +2,7 @@ import os
 import binascii
 
 p = 2 ** 448 - 2 ** 224 - 1
-A24 = 39081
+A24 = 39081                                                                 # A24 = (156326 - 2) / 4    
 base_point = 5
 
 # Defined here https://tools.ietf.org/html/rfc7748
@@ -23,9 +23,9 @@ def X448(k, u):
     z_3 = 1
     swap = 0
 
-    for t in reversed(range(448)):
-        k_t = (k >> t) & 1
-        swap ^= k_t
+    for t in reversed(range(448)):                                             
+        k_t = (k >> t) & 1                                                  # k_1 is either 0 or 1
+        swap ^= k_t                                                         # bitwise XOR with swap = 0
         x_2, x_3 = cswap(swap, x_2, x_3)
         z_2, z_3 = cswap(swap, z_2, z_3)
         swap = k_t
@@ -50,14 +50,14 @@ def X448(k, u):
     return (x_2 * pow(z_2, p - 2, p)) % p
 
 
-def decode_little_endian(b):
-    return sum([b[i] << 8*i for i in range(56)])
+def decode_little_endian(b):                                                # stores bits from lsb to msb
+    return sum([b[i] << 8*i for i in range(56)])                            # u[0] + 256*u[1] + 256^2*u[2] + ... + 256^(n-1)*u[n-1]
 
 
-def decode_scalar448(k):
-  k_list = [(b) for b in k]
-  k_list[0] &= 252
-  k_list[55] |= 128
+def decode_scalar448(k):                                                    # tbd: what the function does
+  k_list = [(b) for b in k]                                                 
+  k_list[0] &= 252                                                          # changes the last two bits to 0
+  k_list[55] |= 128                                                         # changes the first bit to 1
   return decode_little_endian(k_list)
 
 
@@ -66,18 +66,17 @@ def decode_u_coordinate(s):
         raise ValueError('Invalid Curve448 scalar (len=%d)' % len(s))
     return sum(ord(s[i]) << (8 * i) for i in range(56))
 
-# encodeUCoordinate
-def pack(n): 
+def pack(n):                                                                # turns value into string
     return ''.join([chr((n >> (8 * i)) & 255) for i in range(56)])
 
 def clamp(n):
-    n &= ~3
-    n |= 128 << 8 * 55
+    n &= ~3                                                                 # changes last 2 bits to 0
+    n |= 128 << 8 * 55                                                      # changes first bit to 1
     return n
 
 
 # Return nP
-def multscalar(n, p):
+def multscalar(n, p):                                                       # tbd: maybe align parameters with x25519
     n = clamp(decode_scalar448(n))
     p = decode_u_coordinate(p)
     return pack(X448(n, p))
